d7d17d6d86258c6fa6b3e76bc62f85e2c30c2c71
Chandra Shekhar <banothuchandu77@gmail.com>
2026-03-22 23:10:21 +0530

    feat: add address to User and make Order seller optional setup Express server in app.ts and HTTP server with sockets in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from "@prisma/client/extension";
+// index.ts
+import http from "http";
+import app from "./app.js";
+import { initSocket } from "./socket/socket.js";
+import { PrismaClient } from "@prisma/client";
+import type { User } from "./generated/prisma/client.js";
+
 
 const prisma = new PrismaClient();
-type User = Awaited<ReturnType<typeof prisma.user.findUnique>>;
 
+const server = http.createServer(app);
+initSocket(server);
 
-export { prisma, User };
+export { prisma,User };
